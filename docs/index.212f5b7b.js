07334c27076cdb2e628e46fab11ad38cbc3dff6c
Prasanta <7580745+pkd2512@users.noreply.github.com>
2022-03-20 22:20:40 +0530

    fix source map path

--- docs/index.212f5b7b.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello from the console world!");
-//# sourceMappingURL=index.212f5b7b.js.map
--- docs/index.b5fab472.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello from the console world!");
-//# sourceMappingURL=index.b5fab472.js.map
