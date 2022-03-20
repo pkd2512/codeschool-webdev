5d961af42443361c120eb4c81dc299eb1c7b0e7b
Prasanta <7580745+pkd2512@users.noreply.github.com>
2022-03-20 21:13:44 +0530

    first build

---
+++ docs/index.212f5b7b.js
@@ -0,0 +1,2 @@
+console.log("Hello from the console world!");
+//# sourceMappingURL=index.212f5b7b.js.map
---
+++ docs/index.b5fab472.js
@@ -0,0 +1,2 @@
+console.log("Hello from the console world!");
+//# sourceMappingURL=index.b5fab472.js.map
