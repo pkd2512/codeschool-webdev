632604391246929f0bdb3c9214fc18b7bce18541
Prasanta <7580745+pkd2512@users.noreply.github.com>
2022-03-23 02:04:45 +0530

    scrolly setup using enterview js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,8 @@
 // Import all plugins
 import * as bootstrap from 'bootstrap';
 
+import enterViewScrolly from '../scrolly/scrolly';
+
 console.log('Hello from the console world!');
 
+enterViewScrolly('#doge-scrolly')
---
+++ src/scrolly/scrolly.js
@@ -0,0 +1,82 @@
+import './scrolly.css';
+
+/* eslint-disable max-len */
+import enterView from 'enter-view';
+
+/**
+  ---------------
+  VERICAL SCROLLY
+  ---------------
+ * @param {string} scrollerEl - The HTML id of the scroller block.
+ */
+export default enterViewScrolly = (scrollerEl) => {
+  /* Enter Trigger */
+  enterView({
+    selector: `${scrollerEl} .scroll-enter`,
+    // 0 = top of element crosses bottom of viewport (enters screen from bottom).
+    // 1 = top of element crosses top of viewport (exits screen top).
+    offset: 1,
+    enter: (el) => {
+      document
+          .querySelector(`${scrollerEl} .scroll-background`)
+          .classList.remove('unfixed');
+      document
+          .querySelector(`${scrollerEl} .scroll-background`)
+          .classList.add('fixed');
+    },
+    exit: (el) => {
+      document
+          .querySelector(`${scrollerEl} .scroll-background`)
+          .classList.remove('fixed');
+    },
+  });
+
+  /* Exit Trigger */
+  enterView({
+    selector: `${scrollerEl} .scroll-exit`,
+    offset: 0,
+    enter: (el) => {
+      document
+          .querySelector(`${scrollerEl} .scroll-background`)
+          .classList.add('unfixed');
+      document
+          .querySelector(`${scrollerEl} .scroll-background`)
+          .classList.remove('fixed');
+    },
+    exit: (el) => {
+      document
+          .querySelector(`${scrollerEl} .scroll-background`)
+          .classList.remove('unfixed');
+      document
+          .querySelector(`${scrollerEl} .scroll-background`)
+          .classList.add('fixed');
+    },
+  });
+  /*
+    ------------------------------------------
+    Do things here as scroll-sections progress
+    ------------------------------------------
+  */
+  enterView({
+    selector: `${scrollerEl} .anno-block`,
+    enter: (el) => {
+      document
+          .querySelectorAll(`${scrollerEl} .slide`)
+          .forEach((slide) => slide.classList.remove('active'));
+
+      document
+          .querySelector(`${scrollerEl} .slide.${el.id}`)
+          .classList.add('active');
+    },
+    exit: (el) => {
+      const prevNode = document.querySelector(`${scrollerEl} .slide.${el.id}`)
+          .previousElementSibling;
+      if (prevNode) {
+        prevNode.classList.add('active');
+      }
+      document
+          .querySelector(`${scrollerEl} .slide.${el.id}`)
+          .classList.remove('active');
+    },
+  });
+};
